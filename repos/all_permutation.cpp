eee83d4f1ef9edd7772274f87c431f0e5cb6cc7a
kangkanglee <kangkanglee@foxmail.com>
2016-08-16 18:03:23 +0800

    update style

--- repos/all_permutation.cpp
+++ repos/all_permutation.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int total = 0;
 
-void fullPermutation(int * fullArray, int start, int end) { 
+void fullPermutation(int* fullArray, int start, int end) { 
   if (start >= end) {
     for (int i = 0; i <= end; i++) {
       cout << fullArray[i];
@@ -13,25 +13,24 @@ void fullPermutation(int * fullArray, int start, int end) {
   }
   else {
     for (int i = start; i <= end; i++) {
-      swap(fullArray[start], fullArray[i]);//交换  
+      swap(fullArray[start], fullArray[i]); //swap
       fullPermutation(fullArray, start + 1, end);
-      swap(fullArray[start], fullArray[i]);//注意恢复原样  
+      swap(fullArray[start], fullArray[i]); //recover  
     }
   }
 }
 int main()
 {
-  int number;//全排列的长度  
+  int number; // total of numbers
   cout << "Number:" << endl;
   cin >> number;
-  int * fullArray = new int[number];//动态生成全排列的数组  
-                    //初始化  
+  int* fullArray = new int[number]; //allocate memory  
+  // initialize
   for (int i = 0; i < number; i++)
   {
     fullArray[i] = i + 1;
   }
   fullPermutation(fullArray, 0, number - 1);
   cout << "Total = " << total << endl;
-  system("pause");
   return 0;
 }
